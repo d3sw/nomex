28847c06f6df62f944dee0fe0334f362256f90af
Jin Lee <jinslee@gmail.com>
2017-11-18 19:15:05 -0800

    GET requests for /agent

--- lib/nomad/agent.ex
+++ lib/nomad/agent.ex
@@ -1,5 +1,24 @@
 defmodule Nomad.Agent do
+  @health_path "/agent/health"
+  @members_path "/agent/members"
   @self_path "/agent/self"
+  @servers_path "/agent/servers"
+
+  def health do
+    Nomad.request(:get, [@health_path])
+  end
+
+  def health! do
+    Nomad.request!(:get, [@health_path])
+  end
+
+  def members do
+    Nomad.request(:get, [@members_path])
+  end
+
+  def members! do
+    Nomad.request!(:get, [@members_path])
+  end
 
   def self do
     Nomad.request(:get, [@self_path])
@@ -7,5 +26,13 @@ defmodule Nomad.Agent do
 
   def self! do
     Nomad.request!(:get, [@self_path])
+  end
+
+  def servers do
+    Nomad.request(:get, [@servers_path])
+  end
+
+  def servers! do
+    Nomad.request!(:get, [@servers_path])
   end
 end
