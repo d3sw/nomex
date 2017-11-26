06d9aae5b61178b2610f49b435cadf684c90704f
Jin Lee <jinslee@gmail.com>
2017-11-26 10:00:57 -0800

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nomex.Mixfile do
   def project do
     [
       app: :nomex,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
